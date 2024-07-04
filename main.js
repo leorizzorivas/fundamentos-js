c76e1da044d0f7c0fe95190b42af998ebd881b73
leo rizzo <leo.g.rizzorivas@gmail.com>
2024-07-04 19:14:00 -0300

    fundamentos js

---
+++ main.js
@@ -0,0 +1,15 @@
+// fundamentos ejercicio 1 
+/*let nombre = prompt ('ingrese su nombre')
+let apellido = prompt ('ingrese su apellido')
+alert(nombre  + ' '  +  apellido)*/
+// ejercicio 2 
+
+/*let frase = prompt ('ingrese una frase')
+console.log('la longitud de la frase es:' + frase.length)*/
+
+// ejercicio 3 
+
+let palabra1 = prompt ('ingrese una palabra')
+let palabra2 = prompt ('ingrese otra palabra')
+let suma = palabra1 + palabra2
+console.log(suma + 'la longitud de la frase es:'+ suma.length)
